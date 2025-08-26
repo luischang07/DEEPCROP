102adc71d57abfef4a4b00438e6b1955d578c4c4
Luis Lopez <l20170728@culiacan.tecnm.mx>
2025-08-26 13:40:36 -0700

    Add favicon and update app name to DEEPCROP

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,6 +32,11 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        {{-- Favicon --}}
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+        <link rel="icon" type="image/svg+xml" href="{{ asset('logo.svg') }}">
+        <link rel="apple-touch-icon" href="{{ asset('favicon.ico') }}">
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 

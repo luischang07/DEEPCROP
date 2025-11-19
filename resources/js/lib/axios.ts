bb6334019ce3f0e2b49c6bfcb027e61a4cd5c7f4
Luis Lopez <luislopezp913@gmail.com>
2025-11-18 21:35:22 -0600

    fix: axios and duped data.

--- resources/js/lib/axios.ts
+++ resources/js/lib/axios.ts
@@ -4,4 +4,10 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+// Obtener el token CSRF del meta tag
+const token = document.head.querySelector<HTMLMetaElement>('meta[name="csrf-token"]');
+if (token) {
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+}
+
 export default axios;
